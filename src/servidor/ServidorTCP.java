eced3b90c3f16a00f166c48387f969284e38ca27
ACER <xjantonioz@gmail.com>
2026-01-21 17:08:53 +0100

    Creación de la clase servidor (descifrador de mensajes)

--- src/servidor/ServidorTCP.java
+++ src/servidor/ServidorTCP.java
@@ -1,4 +1,37 @@
 package servidor;
 
+import cifrado.CifradoAES;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServidorTCP {
+    private static final int puerto = 5000;
+
+    public static void main(String[] args){
+
+        System.out.println("Servidor iniciando en el puerto: " + puerto);
+
+        try (ServerSocket serverSocket = new ServerSocket(puerto)) {
+            while (true){
+                Socket cliente = serverSocket.accept();
+                System.out.println("Cliente conectado");
+
+                BufferedReader entrada = new BufferedReader(
+                        new InputStreamReader(cliente.getInputStream())
+                );
+
+                String mensajeCifrado = entrada.readLine();
+                String mensajeDescifrado = CifradoAES.descifrar(mensajeCifrado);
+
+                System.out.println("Mensaje recibido (descifrado): " + mensajeDescifrado);
+
+                cliente.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
