6489e31e6f019d2472f27b25340b70ad49895bda
angelgaleraa <angelgs230@gmail.com>
2026-01-21 17:04:29 +0100

    Creacion de la clase para enviar mensajes desde el cliente al servidor

--- src/cliente/ClienteTCP.java
+++ src/cliente/ClienteTCP.java
@@ -1,4 +1,28 @@
 package cliente;
 
+import cifrado.CifradoAES;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClienteTCP {
+    private static final String host = "localhost";
+    private static final int puerto = 5000;
+
+    public static void main(String[] args) {
+        try (
+            Socket socket = new Socket(host, puerto);
+            PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
+            Scanner teclado = new Scanner(System.in);
+        ) {
+            System.out.println("Escribe un mensaje: ");
+            String mensaje = teclado.nextLine();
+            String mensajeCifrado = CifradoAES.cifrar(mensaje);
+            salida.println(mensajeCifrado);
+            System.out.println("Mensaje cifrado enviado al servidor");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
